84a7b1ce28b66e828e0af65d55b500f565642b3d
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-01-08 19:35:55 +0100

    📦 NEW:  add sidebar component and a bit of style.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import { Toaster } from "sonner";
 import { AuthProvider } from "@/app/context/AuthContext";
+import Sidebar from "@/components/layout/Sidebar";
 
 export const metadata: Metadata = {
     title: "Big Meal App",
@@ -17,12 +18,28 @@ export default function RootLayout({
     return (
         <html lang="en">
             {/* AuthProvider pour gérer l'authentification */}
-            <AuthProvider> 
+            {/* <AuthProvider> 
                 <body>
                     <header className="px-4 md:px-6 mx-auto max-w-7xl">
                         <Navbar />
                     </header>
+                    <Sidebar />
                     <main className="p-4 md:p-6 mx-auto max-w-7xl">{children}</main>
+                    <Toaster />
+                </body>
+            </AuthProvider> */}
+            <AuthProvider>
+                <body className="flex flex-col min-h-screen">
+                    <header className="px-4 md:px-6 mx-auto max-w-7xl">
+                        <Navbar />
+                    </header>
+                    <div className="flex flex-1">
+                        {/* Sidebar */}
+                        <aside>
+                            <Sidebar  />
+                        </aside>
+                        <main className="flex-1 p-4 md:p-6">{children}</main>
+                    </div>
                     <Toaster />
                 </body>
             </AuthProvider>
---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { ucFirst } from "@/lib/utils";
+import Link from "next/link";
+import React, { useState } from "react";
+
+const Sidebar = () => {
+        const [active, setActive] = useState(""); // État de la navigation active
+
+        // Liens de navigation
+        const links = [
+            { title: "ingrédients", url: "/ingredients" },
+            { title: "repas", url: "/meals" },
+            { title: "catégorie ingrédient", url: "/categories-ingredient" },
+            { title: "catégorie repas", url: "/categories-meal" },
+            { title: "liste de courses", url: "/shopping-list" },
+        ];
+    return (
+        <div className="w-64 h-screen sticky top-0 flex flex-col p-4">
+            <ul className="space-y-4">
+                {links.map((link) => (
+                    <li key={link.title}>
+                        <Link
+                            href={link.url}
+                            className={`block px-4 py-2 rounded-md ${
+                                active === link.title
+                                    ? "bg-gray-600 text-white font-bold"
+                                    : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                            }`}
+                            onClick={() => setActive(link.title)}
+                        >
+                            {ucFirst(link.title)}
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Sidebar;
