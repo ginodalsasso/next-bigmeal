5e684d6baff1f82017fc973a3c99673fc7733c74
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-02-01 12:21:20 +0100

    🐛 FIX: update Content Security Policy with dynamic nonce and additional directives

--- middleware.ts
+++ middleware.ts
@@ -18,23 +18,30 @@ const dynamicRoutePatterns = [
 
 const publicRoutes = ["/login", "/register", "/"];
 
-// const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-
-// const csp = `
-//     default-src 'self'; 
-//     script-src 'self' 'nonce-${nonce}'; 
-//     style-src 'self' ; 
-//     img-src 'self' data:; 
-//     font-src 'self'; 
-//     connect-src 'self'; 
-//     frame-src 'none'; 
-// `.replace(/\s{2,}/g, ' ').trim(); // Remplace les espaces multiples par un seul espace
+const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
 
 
 export default async function middleware(req: NextRequest) {
+    const csp = `
+        default-src 'self';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: ${
+            process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+        };
+        style-src 'self' 'nonce-${nonce}';
+        img-src 'self' blob: data:;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        upgrade-insecure-requests;
+    `.replace(/\s{2,}/g, ' ').trim(); // Remplace les espaces multiples par un seul espace
+    
+    const requestHeaders = new Headers(req.headers)
+    requestHeaders.set('x-nonce', nonce)
     const path = req.nextUrl.pathname;
 
-    // Vérifie si la route est une route protégée ou publique
+    // Vérifie si la route est une route protégée ou p  ublique
     const isProtectedRoute =
         protectedRoutes.includes(path) || dynamicRoutePatterns.some((pattern) => pattern.test(path));
     const isPublicRoute = publicRoutes.includes(path);
@@ -64,7 +71,7 @@ export default async function middleware(req: NextRequest) {
     }
 
     const response = NextResponse.next();
-    // response.headers.set("Content-Security-Policy", csp);
+    response.headers.set("Content-Security-Policy", csp);
     response.headers.set("X-Content-Type-Options", "nosniff");
     response.headers.set("X-Frame-Options", "DENY");
     response.headers.set("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
