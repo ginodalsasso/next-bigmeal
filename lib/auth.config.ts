1965111e80b2972f193982ae813b11ecc3377f0e
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-02-06 21:35:50 +0100

    📦 NEW: prisma adapter init

--- lib/auth.config.ts
+++ lib/auth.config.ts
@@ -1,5 +1,5 @@
 import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
 export default {
