ee8b62c1ae35808fcc1eb6fc2bc8bee4ca8f787a
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-04-04 19:03:56 +0200

    👌 IMPROVE: - using useFormStatus istead of isLoading in CreatePreparation component. Also Enhance the form for UX and accessibility.

--- app/(views)/meals/_components/(preparation)/CreatePreparation.tsx
+++ app/(views)/meals/_components/(preparation)/CreatePreparation.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 // Bibliothèques tierces
-import React, { useState } from "react";
 import { toast } from "sonner";
 
 // Types et énumérations
@@ -13,10 +12,10 @@ import { preparationConstraints } from "@/lib/constraints/forms_constraints";
 import { useFormValidation } from "@/app/hooks/useFormValidation";
 
 // Composants UI
-import { Button } from "@/components/ui/button";
 import FormErrorMessage from "@/components/forms/FormErrorMessage";
 import { createPreparationAPI } from "@/lib/services/preparation_service";
 import { getCsrfToken } from "next-auth/react";
+import FormSubmitButton from "@/components/forms/FormSubmitButton";
 
 // Services
 
@@ -27,14 +26,6 @@ const CreatePreparation: React.FC<CreatePreparationProps> = ({
     onSubmit,
 }) => {
     // _________________________ HOOKS _________________________
-    
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [form, setForm] = useState<PreparationFormType>({
-        mealId,
-        prepTime: undefined,
-        cookTime: undefined,
-    });
-
     // Hook de validation
     const { error, setError, validate } = useFormValidation<PreparationFormType>(
         preparationConstraints,
@@ -47,24 +38,27 @@ const CreatePreparation: React.FC<CreatePreparationProps> = ({
     // _________________________ LOGIQUE _________________________
 
     // Gestion de la soumission du formulaire
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setIsLoading(true);
-        setError(null); // Réinitialise les erreurs existantes
+    const handleSubmit = async (formData: FormData) => {
+        // Récupérer les données du formulaire
+        const form = {
+            mealId,
+            id: formData.get("id") as string,
+            prepTime: formData.get("prepTime") ? Number(formData.get("prepTime")) : undefined,
+            cookTime: formData.get("cookTime") ? Number(formData.get("cookTime")) : undefined,
+        };
 
         // // Valider les données du formulaire
         if (!validate(form)) {
-            setIsLoading(false);
             return;
         }
 
-        const csrfToken = await getCsrfToken();
-    
-        if (!csrfToken) {
-            console.error("CSRF token invalide");
-            return;
-        }
         try {
+            const csrfToken = await getCsrfToken();
+        
+            if (!csrfToken) {
+                console.error("CSRF token invalide");
+                return;
+            }
             const createdPrepatation = await createPreparationAPI(form, csrfToken);
             onSubmit(createdPrepatation);
 
@@ -72,14 +66,12 @@ const CreatePreparation: React.FC<CreatePreparationProps> = ({
         } catch (error) {
             console.error("[CREATE_PREPARATION_ERROR]", error);
             setError({ general: "Erreur lors de la création de la préparation." });
-        } finally {
-            setIsLoading(false);
         }
     };
 
     // _________________________ RENDU _________________________
     return (
-        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <form className="flex flex-col gap-4" action={handleSubmit}>
             <div className="flex gap-4">
                 {/* Champ pour le temps de préparation du plat */}
                 <div>
@@ -87,12 +79,11 @@ const CreatePreparation: React.FC<CreatePreparationProps> = ({
                         Temps de préparation en minutes (optionnel)
                     </label>
                     <input
+                        className="input-text-select"
                         id="prepTime"
                         type="number"
                         placeholder="60"
-                        value={form.prepTime || ""}
-                        onChange={(e) => setForm({ ...form, prepTime: parseInt(e.target.value) })}
-                        className="input-text-select"
+                        autoComplete="off"
                     />
                 </div>
 
@@ -102,12 +93,11 @@ const CreatePreparation: React.FC<CreatePreparationProps> = ({
                         Temps de cuisson en minutes (optionnel)
                     </label>
                     <input
+                        className="input-text-select"
                         id="cookTime"
                         type="number"
                         placeholder="30"
-                        value={form.cookTime || ""}
-                        onChange={(e) => setForm({ ...form, cookTime: parseInt(e.target.value) })}
-                        className="input-text-select"
+                        autoComplete="off"
                     />
                 </div>
             </div>
@@ -116,9 +106,7 @@ const CreatePreparation: React.FC<CreatePreparationProps> = ({
 
             {/* Bouton de soumission */}
             <div className="flex flex-col-reverse gap-2 lg:justify-end">
-                <Button type="submit" variant="success" disabled={isLoading}>
-                    {isLoading ? "Création de la préparation en cours..." : "Suivant"}
-                </Button>
+                <FormSubmitButton />
             </div>
         </form>
     );
