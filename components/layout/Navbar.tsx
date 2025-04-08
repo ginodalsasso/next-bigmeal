e353bf31935582d855684ef84fdcfe48226eb14d
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-04-08 19:13:16 +0200

    🐛 FIX: Trying to fix Vercel with module not found

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -6,7 +6,7 @@ import { ucFirst } from "@/lib/utils";
 import IsUser from "../isUser";
 import { signOut } from "next-auth/react";
 import IsNotAuthenticated from "../isNotAuthenticated";
-import SearchBar from "@/components/layout/SearchBar";
+ import SearchBar from "./SearchBar";
 import { Carrot, Folder, LogOut, Menu, Search, ShoppingCart, UserRound, Utensils, X } from "lucide-react";
 
 const Navbar = () => {
