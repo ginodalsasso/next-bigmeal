e45c77d09b144a308ccf238449711f945db4fe31
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-04-08 19:48:57 +0200

    🐛 FIX:  Get ingredients and meal in a try and catch.

--- app/(views)/ingredients/page.tsx
+++ app/(views)/ingredients/page.tsx
@@ -12,24 +12,33 @@ interface searchParamsProps {
 }
 
 export default async function IngredientPage({ searchParams }: searchParamsProps) {
-    const params  = await searchParams; // Attendre la résolution de la promesse pour obtenir les paramètres de recherche
-    // Récupérer le numéro de page à partir des paramètres de recherche, ou 1 par défaut
-    const page = parseInt(params.page  || '1') as number; 
-    const itemsPerPage = parseInt(ITEMS_PER_PAGE); // Nombre d'items par page pour la pagination
+    try {
+        const params  = await searchParams; // Attendre la résolution de la promesse pour obtenir les paramètres de recherche
+        // Récupérer le numéro de page à partir des paramètres de recherche, ou 1 par défaut
+        const page = parseInt(params.page  || '1') as number; 
+        const itemsPerPage = parseInt(ITEMS_PER_PAGE); // Nombre d'items par page pour la pagination
 
-    // Paginer avec take=5 ingrédients par page et skip=(page-1)*5 ingrédients
-    // getIngredients(skip, take) : skip = le nombre d'ingrédients à ignorer, take = le nombre d'ingrédients à récupérer
-    const ingredients = await getIngredients((page - 1) * itemsPerPage, itemsPerPage); // page - 1 pour ignorer les ingrédients de la page précédente, 5 pour prendre 5 ingrédients
+        // Paginer avec take=5 ingrédients par page et skip=(page-1)*5 ingrédients
+        // getIngredients(skip, take) : skip = le nombre d'ingrédients à ignorer, take = le nombre d'ingrédients à récupérer
+        const ingredients = await getIngredients((page - 1) * itemsPerPage, itemsPerPage); // page - 1 pour ignorer les ingrédients de la page précédente, 5 pour prendre 5 ingrédients
 
-    return (
-        <div>
-            <IngredientsList fetchedIngredients={ingredients} />
+        return (
+            <div>
+                <IngredientsList fetchedIngredients={ingredients} />
 
-            {/* Pagination */}
-            <Pagination
-                currentPage={page} 
-                hasNextPage={ingredients.length === itemsPerPage} 
-            />
-        </div>
-    );
+                {/* Pagination */}
+                <Pagination
+                    currentPage={page} 
+                    hasNextPage={ingredients.length === itemsPerPage} 
+                />
+            </div>
+        ); 
+    } catch (error) {
+        console.error("Une erreur s'est produite lors de la récupération des ingrédients :", error);
+        return (
+            <div className="flex min-h-screen items-center justify-center text-center">
+                <p className="text-red-500">Erreur lors de la récupération des ingrédients.</p>
+            </div>
+        );
+    }
 }
--- app/(views)/meals/page.tsx
+++ app/(views)/meals/page.tsx
@@ -11,25 +11,34 @@ interface searchParamsProps {
 }
 
 export default async function MealPage({ searchParams }: searchParamsProps) {
-    const params  = await searchParams; // Attendre la résolution de la promesse pour obtenir les paramètres de recherche
-    // Récupérer le numéro de page à partir des paramètres de recherche, ou 1 par défaut
-    const page = parseInt(params.page  || '1') as number; 
+    try {
+        const params  = await searchParams; // Attendre la résolution de la promesse pour obtenir les paramètres de recherche
+        // Récupérer le numéro de page à partir des paramètres de recherche, ou 1 par défaut
+        const page = parseInt(params.page  || '1') as number; 
 
-    const itemsPerPage = parseInt(ITEMS_PER_PAGE); // Nombre d'items par page pour la pagination
-    
-    const meals = await getMeals((page - 1) * itemsPerPage, itemsPerPage);
-    
-    // _________________________ RENDU __________________
-    return (
-        <div>
-            <MealsList fetchedMeals={meals} />
-            
-            {/* Pagination */}
-            <Pagination
-                currentPage={page} 
-                hasNextPage={meals.length === itemsPerPage} 
-            />
-        </div>
+        const itemsPerPage = parseInt(ITEMS_PER_PAGE); // Nombre d'items par page pour la pagination
+        
+        const meals = await getMeals((page - 1) * itemsPerPage, itemsPerPage);
+        
+        // _________________________ RENDU __________________
+        return (
+            <div>
+                <MealsList fetchedMeals={meals} />
+                
+                {/* Pagination */}
+                <Pagination
+                    currentPage={page} 
+                    hasNextPage={meals.length === itemsPerPage} 
+                />
+            </div>
 
-    )   
-}
+        );
+    } catch (error) {
+        console.error("Une erreur s'est produite lors de la récupération des repas :", error);
+        return (
+            <div className="flex min-h-screen items-center justify-center text-center">
+                <p className="text-red-500">Erreur lors de la récupération des ingrédients.</p>
+            </div>
+        );
+    }
+    }
