f03ff5efa53a0cad5aaba3510738e4c4057cb15c
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-04-20 16:33:35 +0200

    🐛 FIX: correction of manifest.json and adding link to website in sw.

--- manifest.ts
+++ manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: "#000000",
         icons: [
             {
-                src: "/public/favicon/favicon192x192.png",
+                src: "/favicon/favicon192x192.png",
                 sizes: "192x192",
                 type: "image/png",
             },
             {
-                src: "/public/favicon/favicon512x512.png",
+                src: "/favicon/favicon512.png",
                 sizes: "512x512",
                 type: "image/png",
             },
