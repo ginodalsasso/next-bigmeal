fd58fc7233571cbb6980fe8b39190ca4ab621e2f
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-04-20 15:11:02 +0200

    📦 NEW: - Manifest and favicons for PWA init.

---
+++ manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: "Big Meal PWA",
+        short_name: "Big Meal",
+        description: "A Progressive Web App built with Next.js",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        icons: [
+            {
+                src: "/public/favicon/favicon192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/public/favicon/favicon512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+        ],
+    };
+}
