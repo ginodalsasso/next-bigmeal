0f224290259c67c8d8a18d28397517f79e0e1372
Dalsasso Gino <159161313+ginodalsasso@users.noreply.github.com>
2025-04-22 18:06:34 +0200

    📦 NEW: Offline page.

---
+++ app/(views)/offline/page.tsx
@@ -0,0 +1,10 @@
+export default function OfflinePage() {
+    return (
+        <div className="flex h-screen flex-col items-center justify-center text-center">
+            <h1>Vous êtes hors ligne</h1>
+            <p>
+                Vérifiez votre connexion Internet ou essayez de recharger la page.
+            </p>
+        </div>
+    );
+}
